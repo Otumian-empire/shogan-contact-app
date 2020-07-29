37f3176733799b28ce5b3ce4bfb09f29f44f90a2
otumian <popecan1000@gmail.com>
2020-07-29 13:42:51 +0000

    added a config file to connect to database and create flask app, ceate virtual environment with pipenev

---
+++ config.py
@@ -0,0 +1,27 @@
+from os import environ
+
+import psycopg2
+from dotenv import load_dotenv
+from flask import Flask
+
+
+# load the .env file and create flask app instance
+load_dotenv('./.env')
+app = Flask(__name__)
+
+# set up the database
+user = environ.get('DB_USER')
+password = environ.get('DB_PASSWORD')
+host = environ.get('DB_HOST')
+port = environ.get('DB_PORT')
+database = environ.get('DB_NAME')
+
+def connect_db():
+    try:
+        conn = psycopg2.connect(
+            dbname=database, user=user, password=password, host=host, port=port)
+        return conn
+    except psycopg2.Error as e:
+        return str(e)
+
+conn = connect_db()
